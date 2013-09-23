8e5ea0f442a115aa738406779e6ea44dde6baa79
liujb <liujiangbei@126.com>
2013-09-23 18:47:43 +0800

    create mongo to crud

---
+++ usemongo/app.js
@@ -0,0 +1,15 @@
+/**
+ * app.js
+ * @authors Your Name (you@example.org)
+ * @date    2013-09-23 18:12:20
+ * @version $Id$
+ */
+
+var api = require("./insert.js");
+api.add(function(err, model) {
+		if (err) {
+			console.log(err);
+			return;
+		} else {}
+	}
+});
--- usemongo/insert.js
+++ usemongo/insert.js
@@ -5,3 +5,31 @@
  * @version $Id$
  */
 
+
+var db = require("./db.js");
+
+exports.add = function(callback) {
+	var model = require('./model.js');
+	if (!model || !db) {
+		callback('Exceptions');
+		return;
+	} else {}
+	db.open(function(err, db) {
+		if (err) {
+			callback(err);
+			return;
+		} else {}
+		db.collection("users", function(err, collection) {
+			if (err) {
+				callback(err);
+				return;
+			} else {}
+
+			//插入记录
+			collection.insert(model, {}, function(err, model) {
+				db.close();
+				callback(err, model);
+			});
+		});
+	});
+};
