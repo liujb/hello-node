5ef775d8c6c601ea0d41c671f88afaed6dbc4f7e
liujb <liujiangbei@126.com>
2013-09-27 00:11:25 +0800

    modify

--- n-blog/app.js
+++ n-blog/app.js
@@ -1,35 +1,39 @@
-
 /**
  * Module dependencies.
  */
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
-
 var app = express();
+
+var MongoStore = require('connect-mongo')(express);
+var settings = require('./settings');
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
+//调用 ejs 模板引擎解析 views/index.ejs
 app.set('view engine', 'ejs');
+
 app.use(express.favicon());
 app.use(express.logger('dev'));
+//connect中间件提供的，用来解析请求体
+//去掉之后命令行不会输出请求的信息
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+//应用路由解析规则
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+	app.use(express.errorHandler());
+} else {}
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+routes(app);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), function() {
+	console.log('Express server listening on port ' + app.get('port'));
 });
---
+++ n-blog/setting.js
@@ -0,0 +1,13 @@
+/**
+ * setting.js
+ * @authors Your Name (you@example.org)
+ * @date    2013-09-26 21:49:39
+ * @version $Id$
+ */
+
+module.exports = {
+	cookieSecret: 'myblog',
+	db: 'blog',
+	host: 'localhost',
+	port: 27017
+};
