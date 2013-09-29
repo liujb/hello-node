6e02e3a23833f6b948010aa6d944c9f781c9a171
liujb <liujiangbei@126.com>
2013-09-29 17:12:16 +0800

    modify

--- n-blog/routes/index.js
+++ n-blog/routes/index.js
@@ -7,28 +7,43 @@ var crypto = require('crypto'),
 
 module.exports = function(app) {
 
-	app.get('/', function(req, res) {
-		res.render('index', {
-			title: '主页',
-			messages: req.flash('info')
-		});
-	});
-	app.get('/reg', function(req, res) {
-		res.render('reg', {
-			title: '注册'
-		});
-	});
-
-	app.get('/flash', function(req, res) {
-		req.flash('info', "Flash is back.");
-		res.redirect('/');
-	});
-
 	/**
-	 * [响应注册事件]
+	 * 首页
 	 * @param  {[type]} req [description]
 	 * @param  {[type]} res [description]
 	 * @return {[type]}     [description]
+	 */
+	app.get('/', function(req, res) {
+		res.render('index', {
+			title: '主页',
+			user: req.session.user,
+			success: req.flash('success').toString(),
+			error: req.flash('error').toString(),
+			messages: req.flash('info').toString()
+		});
+	});
+
+	/**
+	 * 注册页面
+	 */
+	app.get('/reg', checkLogin);
+
+	/**
+	 * 注册页面
+	 */
+	app.get('/reg', function(req, res) {
+		res.render('reg', {
+			title: '注册',
+			user: req.session.user,
+			success: req.flash('success').toString(),
+			error: req.flash('error').toString()
+		});
+	});
+
+	app.post('/reg', checkLogin);
+
+	/**
+	 * 注册
 	 */
 	app.post('/reg', function(req, res) {
 		var name = req.body.name,
@@ -65,17 +80,76 @@ module.exports = function(app) {
 		});
 	});
 
+	app.get('/login', checkLogin);
 	app.get('/login', function(req, res) {
 		res.render('login', {
-			title: '登录'
+			title: '登录',
+			user: req.session.user,
+			success: req.flash('success').toString(),
+			error: req.flash('error').toString()
 		});
 	});
-	app.post('/login', function(req, res) {});
+
+	app.post('/reg', checkLogin);
+	/**
+	 * 登录
+	 */
+	app.post('/login', function(req, res) {
+		var name = req.body.name,
+			pwd = req.body.password,
+			md5 = crypto.createHash('md5'),
+			pwd_md5 = md5.update(pwd).digest('hex');
+		User.get(name, function(err, user) {
+			if (!user) {
+				req.flash('error', '用户不存在.');
+				res.redirect('/');
+			} else {}
+			if (user.password !== pwd_md5) {
+				req.flash('error', '密码不正确.');
+			} else {
+				req.session.user = user;
+				req.flash('success', '登录成功.');
+				res.redirect('/');
+			}
+		});
+	});
+	app.get('/reg', checkLogin);
 	app.get('/post', function(req, res) {
 		res.render('post', {
 			title: '发表'
 		});
 	});
+	app.get('/reg', checkLogin);
 	app.post('/post', function(req, res) {});
-	app.get('/logout', function(req, res) {});
+
+	app.get('/reg', checkLogin);
+	/**
+	 * 注销
+	 */
+	app.get('/logout', function(req, res) {
+		req.session.user = null;
+		req.flash('success', '注销成功.');
+		res.redirect('/');
+	});
+
+
+	function checkNotLogin(req, res, next) {
+		if (!req.session.user) {
+			req.flash('error', '未登录！');
+			res.redirect('/login');
+		} else {
+			next();
+		}
+
+	}
+
+	function checkLogin(req, res, next) {
+		if (req.session.user) {
+			req.flash('error', '已登录.');
+			res.redirect('back');
+		} else {
+			next();
+		}
+
+	}
 };
