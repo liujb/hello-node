ccaaba8dcf2541216ec811111abf4fca8d02f431
liujb <liujiangbei@126.com>
2013-09-29 22:56:53 +0800

    modify post

---
+++ n-blog/models/post.js
@@ -0,0 +1,88 @@
+/**
+ * post.js
+ * @authors Your Name (you@example.org)
+ * @date    2013-09-29 20:52:13
+ * @version $Id$
+ */
+var mongodb = require('./db.js');
+
+var Post = function(obj) {
+	this.title = obj.title;
+	this.content = obj.content;
+	this.author = obj.author;
+};
+
+module.exports = Post;
+Post.fn = Post.prototype;
+
+/**
+ * 发表帖子
+ * @param  {Function} callback [description]
+ * @return {[type]}            [description]
+ */
+Post.fn.save = function(callback) {
+	var date = new Date();
+	//存储各种时间格式，方便以后扩展
+	var time = {
+		date: date,
+		year: date.getFullYear(),
+		month: date.getFullYear() + "-" + (date.getMonth() + 1),
+		day: date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate(),
+		minute: date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes()
+	};
+
+	var post = {
+		title: this.title,
+		content: this.content,
+		author: this.author,
+		time: time
+	};
+
+	mongodb.open(function(err, db) {
+		if (err) {
+			return callback(err);
+		} else {}
+
+		db.collection('post', function(err, coll) {
+			if (err) {
+				mongodb.close();
+				return callback(err);
+			} else {}
+			coll.insert(post, {
+				safe: true
+			}, function(err, result) {
+				mongodb.close();
+				if (err) {
+					return callback(err);
+				} else {
+					callback(null);
+				}
+			});
+		});
+	});
+};
+
+Post.list = function(name, callback) {
+	mongodb.open(function(err, db) {
+		if (err) {
+			mongodb.close();
+			return callback(err);
+		} else {}
+		db.collection('post', function(err, coll) {
+			if (err) {
+				mongodb.close();
+				return callback(err);
+			} else {}
+			var query = {};
+			query.name = name ? name : null;
+			coll.find(query).sort({
+				time: -1
+			}).toArray(function(err, docs) {
+				if (err) {
+					return callback(err);
+				} else {}
+				callback(null, docs); //成功！以数组形式返回查询的结果
+			});
+		});
+	});
+};
--- n-blog/routes/index.js
+++ n-blog/routes/index.js
@@ -3,7 +3,8 @@
  */
 
 var crypto = require('crypto'),
-	User = require('../models/user.js');
+	User = require('../models/user.js'),
+	Post = require('../models/post.js');
 
 module.exports = function(app) {
 
@@ -14,12 +15,17 @@ module.exports = function(app) {
 	 * @return {[type]}     [description]
 	 */
 	app.get('/', function(req, res) {
-		res.render('index', {
-			title: '主页',
-			user: req.session.user,
-			success: req.flash('success').toString(),
-			error: req.flash('error').toString(),
-			messages: req.flash('info').toString()
+		Post.list(null, function(err, posts) {
+			if (err) {
+				posts = [];
+			} else {}
+			res.render('index', {
+				title: '主页',
+				user: req.session.user,
+				posts: posts,
+				success: req.flash('success').toString(),
+				error: req.flash('error').toString()
+			});
 		});
 	});
 
@@ -44,7 +50,7 @@ module.exports = function(app) {
 	});
 
 	/**
-	 * 
+	 *
 	 */
 	app.post('/reg', notLogin_next);
 
@@ -67,7 +73,6 @@ module.exports = function(app) {
 			password: pwd,
 			email: req.body.email
 		});
-		console.log(newUser.email);
 		User.get(newUser.name, function(err, user) {
 			if (user) {
 				req.flash('error', '用户已存在.');
@@ -87,7 +92,7 @@ module.exports = function(app) {
 	});
 
 	/**
-	 * 
+	 *
 	 */
 	app.get('/login', notLogin_next);
 
@@ -107,7 +112,7 @@ module.exports = function(app) {
 	});
 
 	/**
-	 * 
+	 *
 	 */
 	app.post('/login', notLogin_next);
 
@@ -119,12 +124,14 @@ module.exports = function(app) {
 			pwd = req.body.password,
 			md5 = crypto.createHash('md5'),
 			pwd_md5 = md5.update(pwd).digest('hex');
+
 		User.get(name, function(err, user) {
 			if (!user) {
 				req.flash('error', '用户不存在.');
 				res.redirect('/');
+				return; //这个return很有必要
 			} else {}
-			if (user.password !== pwd_md5) {
+			if (user["password"] !== pwd_md5) {
 				req.flash('error', '密码不正确.');
 			} else {
 				req.session.user = user;
@@ -135,7 +142,7 @@ module.exports = function(app) {
 	});
 
 	/**
-	 * 
+	 *
 	 */
 	app.get('/post', login_next);
 
@@ -147,12 +154,15 @@ module.exports = function(app) {
 	 */
 	app.get('/post', function(req, res) {
 		res.render('post', {
-			title: '发表'
+			title: 'Post',
+			user: req.session.user,
+			success: req.flash('success').toString(),
+			error: req.flash('error').toString()
 		});
 	});
 
 	/**
-	 * 
+	 *
 	 */
 	app.get('/post', login_next);
 
@@ -163,11 +173,41 @@ module.exports = function(app) {
 	 * @return {[type]}     [description]
 	 */
 	app.post('/post', function(req, res) {
+		var post = new Post({
+			title: req.body.title,
+			content: req.body.content,
+		});
+		if (!post.title) {
+			req.flash('error', "标题不能为空.");
+			res.redirect('/post');
+			return;
+		} else {}
+		if (!post.content) {
+			req.flash('error', "内容不能为空.");
+			res.redirect('/post');
+			return;
+		} else {}
+
+		var currentUser = req.session.user;
+		if (currentUser && currentUser.name) {
+			post.author = currentUser.name;
+		} else {}
+
+		post.save(function(err) {
+			if (err) {
+				req.flash('error', '发布失败.');
+				return res.redirect('/post');
+			} else {
+				req.flash('success', '发布成功!');
+				res.redirect('/');
+			}
+		});
+
 
 	});
 
 	/**
-	 * 
+	 *
 	 */
 	app.get('/logout', login_next);
 
@@ -189,6 +229,7 @@ module.exports = function(app) {
 	 * @param  {Function} next [description]
 	 * @return {[type]}        [description]
 	 */
+
 	function login_next(req, res, next) {
 		if (!req.session.user) {
 			req.flash('error', '未登录！');
@@ -205,6 +246,7 @@ module.exports = function(app) {
 	 * @param  {Function} next [description]
 	 * @return {[type]}        [description]
 	 */
+
 	function notLogin_next(req, res, next) {
 		if (req.session.user) {
 			req.flash('error', '已登录.');
